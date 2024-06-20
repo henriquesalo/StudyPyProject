ebb8942c1b2dc232bfa8dedf59c328b2659b3083
salomao <henriquersalomao@gmail.com>
2024-06-20 01:30:25 -0300

    project

---
+++ apostilas/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import Apostila, ViewApostila, Tag, Avaliacao
+
+admin.site.register(Apostila)
+admin.site.register(ViewApostila)
+admin.site.register(Tag)
+admin.site.register(Avaliacao)
